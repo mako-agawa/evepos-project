9325a81b3d9ecc784250c835cda25512aad14194
mako-agawa <mako.agawa@gmail.com>
2024-12-07 11:26:33 +0900

    db-setting

--- event-post-api/db/migrate/20241130023146_create_likes.rb
+++ event-post-api/db/migrate/20241130023146_create_likes.rb
@@ -1,16 +1,13 @@
-class CreateLikes < ActiveRecord::Migration[7.2]
+class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.integer :user_id
-      t.integer :event_id
-
+      t.bigint :user_id, null: false
+      t.bigint :event_id, null: false
       t.timestamps
     end
-     # ユニークインデックスで、同じユーザーが同じイベントに複数いいねするのを防ぐ
-     add_index :likes, [:user_id, :event_id], unique: true
 
-     # 外部キーを追加（必要に応じて）
-     add_foreign_key :likes, :users
-     add_foreign_key :likes, :events
+    add_index :likes, [:user_id, :event_id], unique: true
+    add_foreign_key :likes, :users
+    add_foreign_key :likes, :events
   end
 end
