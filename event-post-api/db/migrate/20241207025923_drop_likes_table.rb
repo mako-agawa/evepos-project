310027b894ab3d55336f5923f83322c6fde59e32
mako-agawa <mako.agawa@gmail.com>
2024-12-07 12:04:42 +0900

    drop likes table

---
+++ event-post-api/db/migrate/20241207025923_drop_likes_table.rb
@@ -0,0 +1,17 @@
+class DropLikesTable < ActiveRecord::Migration[7.0]
+  def up
+    drop_table :likes
+  end
+
+  def down
+    create_table :likes do |t|
+      t.bigint :user_id, null: false
+      t.bigint :event_id, null: false
+      t.timestamps
+    end
+
+    add_index :likes, [:user_id, :event_id], unique: true
+    add_foreign_key :likes, :users
+    add_foreign_key :likes, :events
+  end
+end
