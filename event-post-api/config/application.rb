e7d6af56bcec450b05e49e396d68120927bb435b
mako-agawa <mako.agawa@gmail.com>
2024-12-10 18:52:28 +0900

    file

--- event-post-api/config/application.rb
+++ event-post-api/config/application.rb
@@ -1,12 +1,13 @@
-# frozen_string_literal: true
-
 require_relative 'boot'
 require 'rails/all'
 Bundler.require(*Rails.groups)
 
+# ここにdotenvの読み込みを追加
+require 'dotenv'
+Dotenv.load('.env')
+
 module EventPost
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
     config.autoload_lib(ignore: %w[assets tasks])
     config.api_only = true
@@ -14,7 +15,7 @@ module EventPost
     # CORS設定
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000', 'http://18.178.110.119' # フロントエンドのオリジンを許可
+        origins 'http://localhost:3000', 'http://18.178.110.119'
         resource '*',
                  headers: :any,
                  methods: %i[get post put patch delete options head],
--- event-post-api/config/unicorn.rb
+++ event-post-api/config/unicorn.rb
@@ -1,47 +1,30 @@
-# config/unicorn.rb
-# 環境変数からワーカー数を取得（デフォルトは4）
-worker_processes Integer(ENV['WEB_CONCURRENCY'] || 4)
+# /home/ec2-user/evepos-project/event-post-api/config/unicorn.rb
 
-# アプリケーションのルートディレクトリ（デプロイ先のパスに変更）
-app_path = File.expand_path("../../current", __FILE__)
-working_directory app_path
+# プロジェクトのアプリケーションのルートパスを指定
+app_path = '/home/ec2-user/evepos-project/event-post-api'
 
-# ポートの指定 (AWSのロードバランサーからの接続を受ける)
-listen Integer(ENV['PORT'] || 3000), tcp_nopush: true
+worker_processes 2  # プロセス数。CPUコア数に応じて調整
+working_directory app_path  # ここを修正！
 
-# PIDファイルの場所 (AWS EC2での標準的なディレクトリ)
-pid "#{app_path}/tmp/pids/unicorn.pid"
-
-# ログの出力先 (AWSではEC2の/var/log/に出すこともあります)
+# ログの出力先
 stderr_path "#{app_path}/log/unicorn.stderr.log"
 stdout_path "#{app_path}/log/unicorn.stdout.log"
 
-# プリロードでダウンタイムを防ぐ (zero-downtime deployのため)
+# UNIXソケットの指定（Nginxと連携するためのソケット）
+listen "#{app_path}/tmp/sockets/unicorn.sock"
+pid "#{app_path}/tmp/pids/unicorn.pid"
+
+# タイムアウト時間
+timeout 60
+
 preload_app true
 
-# before_forkでActiveRecordの接続を閉じる（重要！）
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
-
-  # ActiveRecordの接続をクローズ
-  if defined?(ActiveRecord::Base)
+  defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
-    puts 'Disconnected from ActiveRecord'
-  end
 end
 
-# after_forkでActiveRecordの接続を再確立
 after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
-
-  # ActiveRecordの接続をリセット (データベース接続を再確立)
-  if defined?(ActiveRecord::Base)
+  defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
-    puts 'Connected to ActiveRecord'
-  end
 end
