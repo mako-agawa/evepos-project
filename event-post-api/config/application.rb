e38e6f557e8d225d45f076f31a8c5c946a959e03
mako-agawa <mako.agawa@gmail.com>
2024-12-19 21:08:04 +0900

    evn check

--- event-post-api/config/application.rb
+++ event-post-api/config/application.rb
@@ -20,6 +20,7 @@ module EventPost
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
+
         resource '*',
                  headers: :any,
                  methods: %i[get post put patch delete options head],
