597cf166d00d1eb19875e3f2304453b925dbd857
mako-agawa <mako.agawa@gmail.com>
2024-12-21 13:11:26 +0900

    clear unicorn

--- event-post-api/config/unicorn.rb
+++ event-post-api/config/unicorn.rb
@@ -1,30 +1,22 @@
-app_path = '/home/ec2-user/evepos-project/event-post-api'
+# unicorn.rb
 
+working_directory "/home/ec2-user/evepos-project/event-post-api"
+
+# PIDファイルのパス
+pid "/home/ec2-user/evepos-project/event-post-api/tmp/pids/unicorn.pid"
+
+stderr_path "/home/ec2-user/evepos-project/event-post-api/log/unicorn.stderr.log"
+stdout_path "/home/ec2-user/evepos-project/event-post-api/log/unicorn.stdout.log"
+
+listen "/home/ec2-user/evepos-project/event-post-api/tmp/sockets/unicorn.sock"
 worker_processes 2
-working_directory app_path
-
-# ログファイルの出力先
-stderr_path File.join(app_path, 'log', 'unicorn.stderr.log')
-stdout_path File.join(app_path, 'log', 'unicorn.stdout.log')
-
-# ソケットファイルの指定
-listen File.join(app_path, 'tmp', 'sockets', 'unicorn.sock'), backlog: 64
-
-# PIDファイルの指定
-pid File.join(app_path, 'tmp', 'pids', 'unicorn.pid')
-
-# Unicornのタイムアウト設定
-timeout 60
-
-# Unicornのプリロードを有効化
+timeout 30
 preload_app true
 
-# フォークの前に実行する処理
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
 end
 
-# フォークの後に実行する処理
 after_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
