85ae8a0bff9ed7fdd866e135212097e47930de39
mako-agawa <mako.agawa@gmail.com>
2024-12-22 15:45:31 +0900

    routes change

--- event-post-api/app/controllers/api/v1/health_checks_controller.rb
+++ event-post-api/app/controllers/api/v1/health_checks_controller.rb
@@ -1,5 +1,10 @@
-class HealthChecksController < ApplicationController
-  def show
-    render json: { status: 'ok' }, status: :ok
+# app/controllers/api/v1/health_checks_controller.rb
+module Api
+  module V1
+    class HealthChecksController < ApplicationController
+      def index
+        render json: { status: 'ok' }, status: :ok
+      end
+    end
   end
 end
