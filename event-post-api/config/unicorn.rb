e5db4d5654d5d1c7a9649fbd9f6ec33825298529
mako-agawa <mako.agawa@gmail.com>
2024-12-24 13:30:39 +0900

    unicorn

--- event-post-api/config/unicorn.rb
+++ event-post-api/config/unicorn.rb
@@ -11,35 +11,19 @@ pid "/home/ec2-user/evepos-project/event-post-api/tmp/pids/unicorn.pid"
 stderr_path "/home/ec2-user/evepos-project/event-post-api/log/unicorn.stderr.log"
 stdout_path "/home/ec2-user/evepos-project/event-post-api/log/unicorn.stdout.log"
 
-# フォーク前の処理
+
 before_fork do |server, worker|
-  # ActiveRecord の接続を切断
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-
-  # 古いプロセスの終了
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exist?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # プロセスが既に存在しない場合は無視
-    end
-  end
-
-  # ソケットファイルが存在する場合は削除
+  # ソケットファイルの削除と作成時の権限設定
   socket_path = "/home/ec2-user/evepos-project/event-post-api/tmp/sockets/unicorn.sock"
   if File.exist?(socket_path)
     File.unlink(socket_path)
   end
+  File.umask(0002) # グループに書き込み権限を与える
 end
 
-# フォーク後の処理
 after_fork do |server, worker|
-  # ActiveRecord の接続を再接続
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
-
-  # ソケットファイルの権限を修正
+  # ソケットファイルの所有者とグループを nginx に設定
   socket_path = "/home/ec2-user/evepos-project/event-post-api/tmp/sockets/unicorn.sock"
-  File.chmod(0775, socket_path)
   File.chown(nil, Process.gid, socket_path)
+  File.chmod(0775, socket_path)
 end
