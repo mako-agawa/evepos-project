7fc6443940995f7c4d1a3b54cdff4392db5f3f75
mako-agawa <mako.agawa@gmail.com>
2024-12-24 13:02:51 +0900

    unicorn

--- event-post-api/config/unicorn.rb
+++ event-post-api/config/unicorn.rb
@@ -1,25 +1,33 @@
-working_directory "/home/ec2-user/evepos-project/event-post-api"
-
-# PIDファイルのパス
-pid "/home/ec2-user/evepos-project/event-post-api/tmp/pids/unicorn.pid"
-
-# ログファイルのパス
-stderr_path "/home/ec2-user/evepos-project/event-post-api/log/unicorn.stderr.log"
-stdout_path "/home/ec2-user/evepos-project/event-post-api/log/unicorn.stdout.log"
-
 # ソケットの設定
 listen "/home/ec2-user/evepos-project/event-post-api/tmp/sockets/unicorn.sock"
 worker_processes 2
 timeout 30
 preload_app true
 
+# PIDファイル
+pid "/home/ec2-user/evepos-project/event-post-api/tmp/pids/unicorn.pid"
+
+# ログファイル
+stderr_path "/home/ec2-user/evepos-project/event-post-api/log/unicorn.stderr.log"
+stdout_path "/home/ec2-user/evepos-project/event-post-api/log/unicorn.stdout.log"
+
 # フォーク前の処理
 before_fork do |server, worker|
   # ActiveRecord の接続を切断
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
 
+  # 古いプロセスの終了
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exist?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # プロセスが既に存在しない場合は無視
+    end
+  end
+
   # ソケットファイルの権限を修正
-  File.chmod(0777, "/home/ec2-user/evepos-project/event-post-api/tmp/sockets/unicorn.sock")
+  File.chmod(0775, "/home/ec2-user/evepos-project/event-post-api/tmp/sockets/unicorn.sock")
 end
 
 # フォーク後の処理
