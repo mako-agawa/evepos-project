2ae2fefd38dd5408e45beb102caaa3d80825dd4e
mako-agawa <mako.agawa@gmail.com>
2024-12-25 08:14:00 +0900

    puma

---
+++ event-post-api/config/puma.rb
@@ -0,0 +1,17 @@
+# ソケットの設定
+bind "unix:///home/ec2-user/evepos-project/event-post-api/tmp/sockets/puma.sock"
+
+# PIDファイル
+pidfile "/home/ec2-user/evepos-project/event-post-api/tmp/pids/puma.pid"
+
+# スレッドとワーカーの設定
+threads 1, 5
+workers 2
+
+# ログファイル
+stdout_redirect "/home/ec2-user/evepos-project/event-post-api/log/puma.stdout.log", "/home/ec2-user/evepos-project/event-post-api/log/puma.stderr.log", true
+
+# デーモン化
+daemonize true
+
+preload_app!
