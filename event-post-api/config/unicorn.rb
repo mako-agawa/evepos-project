ee35eedcf04716b539944cd70a83f7a8487ad209
mako-agawa <mako.agawa@gmail.com>
2024-12-24 13:46:34 +0900

    unicorn

--- event-post-api/config/unicorn.rb
+++ event-post-api/config/unicorn.rb
@@ -13,12 +13,20 @@ stdout_path "/home/ec2-user/evepos-project/event-post-api/log/unicorn.stdout.log
 
 
 before_fork do |server, worker|
-  # ソケットファイルの削除と作成時の権限設定
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exist?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # プロセスが存在しない場合は無視
+    end
+  end
+
+  # ソケットファイルが存在する場合は削除
   socket_path = "/home/ec2-user/evepos-project/event-post-api/tmp/sockets/unicorn.sock"
   if File.exist?(socket_path)
     File.unlink(socket_path)
   end
-  File.umask(0002) # グループに書き込み権限を与える
 end
 
 after_fork do |server, worker|
