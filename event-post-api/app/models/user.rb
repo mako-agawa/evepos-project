f66f28101e5144074348723c3b6c2ed0e6d5f512
mako-agawa <mako.agawa@gmail.com>
2024-12-26 08:00:43 +0900

    aaaa

--- event-post-api/app/models/user.rb
+++ event-post-api/app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+  
   before_create :generate_authentication_token
 
   has_many :events, dependent: :destroy  # この行を追加
