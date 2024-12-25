784271d7f4283a5bf3fa0ecee0aa2ddcf895f730
mako-agawa <mako.agawa@gmail.com>
2024-12-25 15:58:58 +0900

    ssss

--- event-post-api/config/puma.rb
+++ event-post-api/config/puma.rb
@@ -1,6 +1,4 @@
-# ソケットの設定
-# bind "unix:///home/ec2-user/evepos-project/event-post-api/tmp/sockets/puma.sock"
-# TCPポートでのバインドに変更
+# TCPポートでのバインド
 bind "tcp://0.0.0.0:3000"
 
 # PIDファイル
@@ -22,3 +20,30 @@ on_worker_boot do
   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
   ActiveRecord::Base.establish_connection
 end
+
+# worker 停止時の設定
+on_worker_shutdown do
+  Rails.logger.info("Worker shutting down...")
+end
+
+# Puma クラスタ起動前の設定
+before_fork do
+  Rails.logger.info("Puma master process starting...")
+end
+
+# Puma クラスタ終了時の設定
+after_worker_fork do
+  Rails.logger.info("Puma worker process forked...")
+end
+
+# Puma サーバの状態を確認するためのコントロールアプリケーションの設定 (必要に応じて有効化)
+activate_control_app "tcp://0.0.0.0:9293", { no_token: true }
+
+# タイムアウト設定 (長時間リクエストでのタイムアウトを防ぐ)
+worker_timeout 60
+
+# プロセス名のタグ付け
+tag "event-post-api"
+
+# 必要に応じて再起動時の設定 (RubyのGCに関連するもの)
+prune_bundler
