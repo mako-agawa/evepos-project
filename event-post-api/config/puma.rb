8f6c1412c4144dea9bcb82647dba424e3d342896
mako-agawa <mako.agawa@gmail.com>
2024-12-28 16:33:04 +0900

    re puma

--- event-post-api/config/puma.rb
+++ event-post-api/config/puma.rb
@@ -1,49 +1,12 @@
-# TCPポートでのバインド
-bind "tcp://0.0.0.0:3001"
+workers ENV.fetch('WEB_CONCURRENCY') { 2 }
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
+threads threads_count, threads_count
 
-# PIDファイル
-pidfile "/home/ec2-user/evepos-project/event-post-api/tmp/pids/puma.pid"
-
-# スレッドとワーカーの設定
-threads 1, 5
-workers 2
-
-# ログファイル
-stdout_redirect "/home/ec2-user/evepos-project/event-post-api/log/puma.stdout.log", "/home/ec2-user/evepos-project/event-post-api/log/puma.stderr.log", true
-
-# preload_app を有効化
 preload_app!
 
-# worker 起動時の設定
-on_worker_boot do
-  require "active_record"
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection
-end
+# rackup DefaultRackup は削除
+port ENV.fetch('PORT') { 3001 }
+environment ENV.fetch('RAILS_ENV') { 'production' }
 
-# worker 停止時の設定
-on_worker_shutdown do
-  Rails.logger.info("Worker shutting down...")
-end
-
-# Puma クラスタ起動前の設定
-before_fork do
-  Rails.logger.info("Puma master process starting...")
-end
-
-# Puma クラスタ終了時の設定
-after_worker_fork do
-  Rails.logger.info("Puma worker process forked...")
-end
-
-# Puma サーバの状態を確認するためのコントロールアプリケーションの設定 (必要に応じて有効化)
-activate_control_app "tcp://0.0.0.0:9293", { no_token: true }
-
-# タイムアウト設定 (長時間リクエストでのタイムアウトを防ぐ)
-worker_timeout 60
-
-# プロセス名のタグ付け
-tag "event-post-api"
-
-# 必要に応じて再起動時の設定 (RubyのGCに関連するもの)
-prune_bundler
+pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
+plugin :tmp_restart
