db4cdda27eae52d6231083ca499c669fccb01ea5
mako-agawa <mako.agawa@gmail.com>
2024-12-29 01:03:34 +0900

    a

--- event-post-api/config/environments/production.rb
+++ event-post-api/config/environments/production.rb
@@ -3,58 +3,40 @@
 require 'active_support/core_ext/numeric/bytes'
 
 Rails.application.configure do
-  # Rails の master.key または RAILS_MASTER_KEY を強制的に使用
   config.require_master_key = true
-
-  # eager_loadをtrueにして、すべてのアプリケーションのコードが起動時にロードされるようにする
   config.eager_load = true
 
+  # SSLの設定 (ALBでSSLを管理する場合はコメントアウトのまま)
   # config.force_ssl = true
 
   config.secret_key_base = ENV["SECRET_KEY_BASE"] || Rails.application.credentials.secret_key_base
-
-  # ログのフォーマット設定
   config.logger = ActiveSupport::Logger.new(Rails.root.join('log', 'production.log'), 1, 50.megabytes)
   config.log_tags = [:request_id]
   config.log_level = :debug
 
-  # ホストの許可
-  config.hosts << '18.178.110.119'  # EC2のパブリックIPアドレス
-  config.hosts << '127.0.0.1'       # ローカルホスト(IPv4)の許可
-  config.hosts << '::1'             # ローカルホスト(IPv6)の許可
-  config.hosts << 'api.evepos.net'  # APIのドメインを許可
+  config.hosts << '18.178.110.119'
+  config.hosts << '127.0.0.1'
+  config.hosts << '::1'
+  config.hosts << 'api.evepos.net'
   config.hosts << "evepos-elb-1733878306.ap-northeast-1.elb.amazonaws.com"
 
-  # ホストの認証をカスタマイズ
   config.host_authorization = {
     exclude: ->(request) {
       request.path == '/up' ||
       ['127.0.0.1', '::1', '18.178.110.119'].include?(request.remote_ip) ||
-      request.host == 'api.evepos.net'  # ホスト名で許可
+      request.host == 'api.evepos.net'
     }
   }
 
-  # 静的ファイルの配信を無効化
   config.public_file_server.enabled = false
-
-  # ActionMailerの設定
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = false
 
-  # ActiveStorageの設定（必要に応じてアンコメント）
   # config.active_storage.service = :local
 
-  # Railsの不要なログを削減する
   config.active_record.verbose_query_logs = false
   config.active_record.dump_schema_after_migration = false
   config.active_record.logger = nil
-
-  # 本番環境のSSLリダイレクトを無効化（ロードバランサーがSSLを管理する場合）
-  # config.force_ssl = false
-
-  # I18nのフォールバックを有効化
   config.i18n.fallbacks = true
-
-  # アクティブサポートのデプリケーションログを表示しない
   config.active_support.report_deprecations = false
 end
