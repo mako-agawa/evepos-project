68389a75c2cc52cce252b34c81575dc35392452e
mako-agawa <mako.agawa@gmail.com>
2024-12-28 17:50:32 +0900

    tcp

--- event-post-api/config/puma.rb
+++ event-post-api/config/puma.rb
@@ -6,7 +6,7 @@ preload_app!
 
 # Pumaが待ち受けるポートを指定
 # 既存のポート設定を削除し、SSLバインディングを記述
-bind 'ssl://127.0.0.1:3001?key=/etc/letsencrypt/live/api.evepos.net/privkey.pem&cert=/etc/letsencrypt/live/api.evepos.net/fullchain.pem'
+bind 'tcp://127.0.0.1:3001'
 
 # 環境設定
 environment ENV.fetch('RAILS_ENV') { 'production' }
