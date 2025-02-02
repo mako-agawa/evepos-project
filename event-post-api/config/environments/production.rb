1c039e233c4b49ff661f357f69fa644dbb509575
Makoto_Agawa <51506765+mako-agawa@users.noreply.github.com>
2025-02-02 21:50:03 +0900

    s3

--- event-post-api/config/environments/production.rb
+++ event-post-api/config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   Rails.application.configure do
     config.active_storage.service = :amazon
     config.active_storage.resolve_model_to_route = :rails_storage_redirect
-    config.active_storage.routes_prefix = '/rails/active_storage'
-    config.active_storage.default_url_options = { host: 'https://api.evepos.net' }
+    # config.active_storage.routes_prefix = '/rails/active_storage'
+    # config.active_storage.default_url_options = { host: 'https://api.evepos.net' }
   end
 end
